6a181a5f09d8b418ef20647d6ff1044024017dbc
carlosmoshudev <carlos@carlosmoshu.com>
2020-09-23 08:43:30 +0200

    Create modulo-generador.vb

---
+++ modulo-generador.vb
@@ -0,0 +1,26 @@
+sub mail_generator()
+
+end sub
+
+sub notepad_resume()
+
+end sub
+
+'mail validations
+
+sub get_emails()
+
+end sub
+
+function ripEmails(mails, Optional separador = ";")
+
+end function
+
+Function isValidEmail(strEmail)
+
+
+End Function
+
+Function sortArray(myArray As Variant)
+
+End Function
